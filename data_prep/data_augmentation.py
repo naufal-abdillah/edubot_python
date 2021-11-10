3440b1e68917cc6ba3b1d3f9cc0150bad8e90c31
naufal-abdillah <abdillahnfl@gmail.com>
2021-11-10 14:24:15 +0700

    remove unnecessary imports

--- data_prep/data_augmentation.py
+++ data_prep/data_augmentation.py
@@ -1,9 +1,6 @@
-import keras_ocr
-import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 import cv2
 
 BATCH_SIZE = 100
